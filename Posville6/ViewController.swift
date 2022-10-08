ed7de14e8d083b69207c9f76d4a82a876bf05096
YunJoon <kpl88@naver.com>
2022-10-08 23:41:55 +0900

    first init

---
+++ Posville6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Posville6
+//
+//  Created by Chicken on 2022/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
