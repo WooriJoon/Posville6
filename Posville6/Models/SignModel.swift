666d21cb1061885efac22f833327a587cb21adf2
YunJoon <kpl88@naver.com>
2022-10-09 18:10:24 +0900

    [Feat] Add Sign Model

---
+++ Posville6/Models/SignModel.swift
@@ -0,0 +1,72 @@
+//
+//  SignModel.swift
+//  Posville6
+//
+//  Created by Chicken on 2022/10/09.
+//
+
+import Foundation
+
+extension Quiz {
+    static let signQuizzes: [Quiz] = [
+        // 10
+        Quiz(level: .Beginning, question: "mg", options: ["밀리그램", "메가그램"], rightAnswer: "밀리그램"),
+        Quiz(level: .Beginning, question: "t", options: ["톤", "티"], rightAnswer: "톤"),
+        Quiz(level: .Beginning, question: "㎖", options: ["밀리리터", "마일"], rightAnswer: "밀리리터"),
+        Quiz(level: .Beginning, question: "mm", options: ["밀리미터", "미리미터"], rightAnswer: "밀리미터"),
+        Quiz(level: .Beginning, question: "㎗", options: ["데시리터", "델리"], rightAnswer: "데시리터"),
+        Quiz(level: .Beginning, question: "m³", options: ["세제곱미터", "쓰리미터"], rightAnswer: "세제곱미터"),
+        Quiz(level: .Beginning, question: "ft", options: ["피트", "피쳐링"], rightAnswer: "피트"),
+        Quiz(level: .Beginning, question: "yd", options: ["야드", "와이드"], rightAnswer: "야드"),
+        Quiz(level: .Beginning, question: "oz", options: ["온스", "오즈"], rightAnswer: "온스"),
+        Quiz(level: .Beginning, question: "mach", options: ["마하", "매치"], rightAnswer: "마하"),
+        
+        // 20
+        Quiz(level: .Beginning, question: "KB", options: ["킬로바이트", "킬로비트"], rightAnswer: "킬로바이트"),
+        Quiz(level: .Beginning, question: "gal", options: ["갤런", "갤리"], rightAnswer: "갤런"),
+        Quiz(level: .Beginning, question: "TB", options: ["테라바이트", "텍스바이트"], rightAnswer: "테라바이트"),
+        Quiz(level: .Beginning, question: "hPa", options: ["헥토파스칼", "헥파스칼"], rightAnswer: "헥토파스칼"),
+        Quiz(level: .Beginning, question: "Ω", options: ["옴", "암"], rightAnswer: "옴"),
+        Quiz(level: .Beginning, question: "1000mm =", options: ["1m", "10m"], rightAnswer: "1m"),
+        Quiz(level: .Beginning, question: "™", options: ["상표 기호", "수심 깊이"], rightAnswer: "상표 기호"),
+        Quiz(level: .Beginning, question: "♂︎", options: ["남자", "여자"], rightAnswer: "남자"),
+        Quiz(level: .Beginning, question: "( )", options: ["소괄호", "중괄호"], rightAnswer: "소괄호"),
+        Quiz(level: .Beginning, question: "{ }", options: ["대괄호", "중괄호"], rightAnswer: "대괄호"),
+        
+        // 30
+        Quiz(level: .Beginning, question: "♖", options: ["룩", "비숍"], rightAnswer: "룩"),
+        Quiz(level: .Beginning, question: "☢︎", options: ["방사능", "바람"], rightAnswer: "방사능"),
+        Quiz(level: .Beginning, question: "🔞", options: ["18세 미만", "18세 이하"], rightAnswer: "18세 미만"),
+        Quiz(level: .Beginning, question: "🅏", options: ["화장실", "회사"], rightAnswer: "화장실"),
+        Quiz(level: .Beginning, question: "K", options: ["칼륨", "칼슘"], rightAnswer: "칼륨"),
+        Quiz(level: .Beginning, question: "Ca", options: ["칼슘", "칼륨"], rightAnswer: "칼슘"),
+        Quiz(level: .Beginning, question: "H", options: ["수소", "황산"], rightAnswer: "수소"),
+        Quiz(level: .Beginning, question: "Fe", options: ["철", "납"], rightAnswer: "철"),
+        Quiz(level: .Beginning, question: "Pb", options: ["납", "파리바게뜨"], rightAnswer: "납"),
+        Quiz(level: .Beginning, question: "He", options: ["헬륨", "수소"], rightAnswer: "헬륨"),
+        
+        // 40
+        Quiz(level: .Beginning, question: "P", options: ["인", "납"], rightAnswer: "인"),
+        Quiz(level: .Beginning, question: "Mg", options: ["마그네슘", "밀리그램"], rightAnswer: "마그네슘"),
+        Quiz(level: .Beginning, question: "Ne", options: ["네온", "나트륨"], rightAnswer: "네온"),
+        Quiz(level: .Beginning, question: "&", options: ["엔퍼샌드", "퍼샌드"], rightAnswer: "엔퍼샌드"),
+        Quiz(level: .Beginning, question: "♬", options: ["16분음표", "8분음표"], rightAnswer: "16분음표"),
+        Quiz(level: .Beginning, question: "♭", options: ["플랫", "샵"], rightAnswer: "플랫"),
+        Quiz(level: .Beginning, question: "𝄞", options: ["높은음자리표", "낮은음자리표"], rightAnswer: "높은음자리표"),
+        Quiz(level: .Beginning, question: "ㆁ", options: ["옛이응", "아래 아"], rightAnswer: "옛이응"),
+        Quiz(level: .Beginning, question: "￥", options: ["엔", "유로"], rightAnswer: "엔"),
+        Quiz(level: .Beginning, question: "$", options: ["달러", "유로"], rightAnswer: "달러"),
+        
+        // 50
+        Quiz(level: .Beginning, question: "€", options: ["유로", "엔"], rightAnswer: "유로"),
+        Quiz(level: .Beginning, question: "IV", options: ["4", "6"], rightAnswer: "4"),
+        Quiz(level: .Beginning, question: "IX", options: ["9", "11"], rightAnswer: "9"),
+        Quiz(level: .Beginning, question: "㎐", options: ["헤르쯔", "헥토파스칼"], rightAnswer: "헤르쯔"),
+        Quiz(level: .Beginning, question: "√", options: ["루트", "나누기"], rightAnswer: "루트"),
+        Quiz(level: .Beginning, question: "Σ", options: ["대괄호", "시그마"], rightAnswer: "시그마"),
+        Quiz(level: .Beginning, question: "π", options: ["파이", "ㅠㅠ"], rightAnswer: "파이"),
+        Quiz(level: .Beginning, question: "♲", options: ["방사능", "재활용"], rightAnswer: "재활용"),
+        Quiz(level: .Beginning, question: "♤", options: ["스페이드", "클로버"], rightAnswer: "스페이드"),
+        Quiz(level: .Beginning, question: "℃", options: ["섭씨", "화씨"], rightAnswer: "섭씨")
+    ]
+}
