00aa70e256a03c599d4c02430c11fbf3b2b946df
HyeonSoo Kim <95853235+BrightHyeon@users.noreply.github.com>
2022-10-15 20:26:35 +0900

    [Feat] Send User Info PlayerVC to LoserVC
    And setting user images in imageViews

--- Posville6/Controllers/LoserSettingViewController.swift
+++ Posville6/Controllers/LoserSettingViewController.swift
@@ -20,6 +20,10 @@ class LoserSettingViewController: UIViewController {
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var loserCountLabel: UILabel!
     
+    lazy var playerImages: [UIImageView] = [
+        player0Image, player1Image, player2Image, player3Image, player4Image, player5Image
+    ]
+    
     var category: Category = .all
     var gameMode: GameMode = .normal
     var playerIndex: [Int]?
@@ -27,15 +31,20 @@ class LoserSettingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        
-//        [player0Image, player1Image, player2Image, player3Image, player4Image, player5Image]
-//        [0, 2, 4, 5]
+    }
+}
+
+// MARK: UI Setup related Methods.
+// UI 초기설정과 관련된 메서드들입니다.
+private extension LoserSettingViewController {
+    func setupUI() {
+        sheetSetup()
+        imageSetup()
     }
     
-    func setupUI() {
+    func sheetSetup() {
         categoryLabel.text = "\(category.rawValue) - \(gameMode.rawValue)"
-        loserCountLabel.text = "탈락자 수 1명"
-        
+        loserCountLabel.text = gameMode == .normal ? "탈락자 수 1명" : "사이클 수 1번"
         
         loserCountSheet.layer.shadowOpacity = 0.3
         loserCountSheet.layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
@@ -44,12 +53,17 @@ class LoserSettingViewController: UIViewController {
     }
     
     func imageSetup() {
-        
+        for idx in playerIndex! {
+            playerImages[idx].image = UIImage(named: "player\(idx)")
+        }
     }
-    
+}
+
+// MARK: IBAction Methods.
+// 버튼 액션과 관련된 메서드들입니다.
+extension LoserSettingViewController {
     @IBAction func loserCountStepperTapped(_ sender: UIStepper) {
         let currentValue = Int(sender.value)
-        
         switch gameMode {
         case .normal:
             sender.maximumValue = Double(self.playerIndex!.count) - 1
@@ -67,5 +81,4 @@ class LoserSettingViewController: UIViewController {
     // TODO: currentValue 넘기기, 3초 셀때 문제 가져오기
     @IBAction func startButtonTapped(_ sender: UIButton) {
     }
-    
 }
