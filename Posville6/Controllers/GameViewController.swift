e336ae0e0594ca3835ab52729cb69d1c838b8946
Hyeonmin Kim <hminkim2@gmail.com>
2022-10-16 18:57:31 +0900

    [Style] GameViewController 로직 extention으로 정리

--- Posville6/Controllers/GameViewController.swift
+++ Posville6/Controllers/GameViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class GameViewController: UIViewController {
 	
-    var quizManager = QuizManager.shared
-    var quizzes: [Quiz] = []
-    
+	var quizManager = QuizManager.shared
+	var quizzes: [Quiz] = []
+	
 	@IBOutlet weak var player0Image: UIImageView!
 	@IBOutlet weak var player1Image: UIImageView!
 	@IBOutlet weak var player2Image: UIImageView!
@@ -28,103 +28,110 @@ class GameViewController: UIViewController {
 	@IBOutlet weak var button3_1: UIButton!
 	@IBOutlet weak var button3_2: UIButton!
 	@IBOutlet weak var button3_3: UIButton!
-    
-    var category: Category = .all
-    var gameMode: GameMode = .normal
-    var playerIndex: [Int]?
-    var loserCount: Int?
-    var currentLoserCount: Int?
-    
-    lazy var playerImages: [UIImageView] = [
-        player0Image, player1Image, player2Image, player3Image, player4Image, player5Image
-    ]
+	
+	var category: Category = .all
+	var gameMode: GameMode = .normal
+	var playerIndex: [Int]?
+	var loserCount: Int?
+	var currentLoserCount: Int?
+	
+	lazy var playerImages: [UIImageView] = [
+		player0Image, player1Image, player2Image, player3Image, player4Image, player5Image
+	]
 	
 	@IBOutlet weak var timeBar: UIProgressView!
 	
 	var timer = Timer()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        timeBar.progress = 0.0
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		timeBar.progress = 0.0
 		setupUI()
-        setupQuiz()
-        setupQuestionView()
-        imageSetup()
-    }
-    
-    func setupUI() {
-        // 문제를 띄워주는 view에 그림자 효과
-        questionView.layer.shadowOpacity = 0.3
-        questionView.layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
-        questionView.layer.shadowRadius = 4
-        questionView.layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
-    }
-    
-    func setupQuiz() {
-        quizzes = quizManager.fetchQuizzes(category: category).shuffled()
-    }
-    
-    func setupQuestionView() {
-        guard let quiz = quizzes.popLast() else {
-            print("Quiz Error")
-            return
-        }
-        
-        switch quiz.options.count {
-        case 2:
-            threeButtonView.alpha = 0
-            threeButtonView.isUserInteractionEnabled = false
-            
-            if quiz.question == "" {
-                questionLabel.text = "다음 중 맞는 것은?"
-            } else {
-                questionLabel.text = quiz.question
-            }
-            
-            let options = quiz.options.shuffled()
-            setAnswerButton(option: options[0], button: button2_1)
-            setAnswerButton(option: options[1], button: button2_2)
-            
-        case 3:
-            twoButtonView.alpha = 0
-            twoButtonView.isUserInteractionEnabled = false
-            
-            if quiz.question == "" {
-                questionLabel.text = "다음 중 맞는 것은?"
-            } else {
-                questionLabel.text = quiz.question
-            }
-            
-            let options = quiz.options.shuffled()
-            setAnswerButton(option: options[0], button: button3_1)
-            setAnswerButton(option: options[1], button: button3_2)
-            setAnswerButton(option: options[2], button: button3_3)
-            
-        default:
-            print("Quiz options Error")
-            return
-        }
-//        view.setNeedsLayout()
-    }
-    
-    // 선택된 플레이어의 자리에만 플레이어 이미지를 보여줍니다
-    func imageSetup() {
-        for idx in playerIndex! {
-            playerImages[idx].image = UIImage(named: "player\(idx)")
-        }
-    }
-    
-    func setAnswerButton(option: String, button: UIButton) {
-        button.setTitle(option, for: .normal)
-        button.layer.shadowOpacity = 0.3
-        button.layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
-        button.layer.shadowRadius = 4
-        button.layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
-    }
+		setupQuiz()
+		setupQuestionView()
+		imageSetup()
+	}
+	
+	func setupUI() {
+		// 문제를 띄워주는 view에 그림자 효과
+		questionView.layer.shadowOpacity = 0.3
+		questionView.layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
+		questionView.layer.shadowRadius = 4
+		questionView.layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
+	}
+	
+	func setupQuiz() {
+		quizzes = quizManager.fetchQuizzes(category: category).shuffled()
+	}
+	
+	func setupQuestionView() {
+		guard let quiz = quizzes.popLast() else {
+			print("Quiz Error")
+			return
+		}
+		
+		switch quiz.options.count {
+		case 2:
+			threeButtonView.alpha = 0
+			threeButtonView.isUserInteractionEnabled = false
+			
+			if quiz.question == "" {
+				questionLabel.text = "다음 중 맞는 것은?"
+			} else {
+				questionLabel.text = quiz.question
+			}
+			
+			let options = quiz.options.shuffled()
+			setAnswerButton(option: options[0], button: button2_1)
+			setAnswerButton(option: options[1], button: button2_2)
+			
+		case 3:
+			twoButtonView.alpha = 0
+			twoButtonView.isUserInteractionEnabled = false
+			
+			if quiz.question == "" {
+				questionLabel.text = "다음 중 맞는 것은?"
+			} else {
+				questionLabel.text = quiz.question
+			}
+			
+			let options = quiz.options.shuffled()
+			setAnswerButton(option: options[0], button: button3_1)
+			setAnswerButton(option: options[1], button: button3_2)
+			setAnswerButton(option: options[2], button: button3_3)
+			
+		default:
+			print("Quiz options Error")
+			return
+		}
+		//        view.setNeedsLayout()
+	}
+	
+	// 선택된 플레이어의 자리에만 플레이어 이미지를 보여줍니다
+	func imageSetup() {
+		for idx in playerIndex! {
+			playerImages[idx].image = UIImage(named: "player\(idx)")
+		}
+	}
+	
+	func setAnswerButton(option: String, button: UIButton) {
+		button.setTitle(option, for: .normal)
+		button.layer.shadowOpacity = 0.3
+		button.layer.shadowOffset = CGSize(width: 1.0, height: 4.0)
+		button.layer.shadowRadius = 4
+		button.layer.shadowColor = UIColor.black.withAlphaComponent(0.25).cgColor
+	}
+}
+
+// MARK: Progress Bar
+extension GameViewController {
 	
 	// TODO: button3_2 버튼을 눌렀을 때 액션이 되게 임시로 설정해놨는데 추후 뷰가 변경되었을 때 액션하도록 변경
 	@IBAction func buttonPressForTest(_ sender: Any) {
-		
+		progressBar()
+	}
+	
+	func progressBar() {
 		var progress: Float = 0.0
 		timeBar.progress = progress
 		
@@ -140,14 +147,21 @@ class GameViewController: UIViewController {
 			}
 		})
 	}
-	  
-    // TODO: 일시정지 버튼 Alert 구현
-    @IBAction func pauseButtonTapped(_ sender: UIButton) {
-//        let alert = UIAlertController(title: "일시정지", message: "게임에서 정말 나가시겠습니까?", preferredStyle: .alert)
-//        let cancel = UIAlertAction(title: "취소", style: .default)
-//        let exit = UIAlertAction(title: "나가기", style: .cancel)
-//        alert.addAction(exit)
-//        alert.addAction(cancel)
-//        present(alert, animated: true)
-    }
+}
+
+// MARK: Pause
+extension GameViewController {
+	
+	@IBAction func pauseButtonTapped(_ sender: UIButton) {
+		pause()
+	}
+	
+	func pause() {
+		let alert = UIAlertController(title: "일시정지", message: "게임에서 정말 나가시겠습니까?", preferredStyle: .alert)
+		let cancel = UIAlertAction(title: "취소", style: .default)
+		let exit = UIAlertAction(title: "나가기", style: .cancel)
+		alert.addAction(exit)
+		alert.addAction(cancel)
+		present(alert, animated: true)
+	}
 }
